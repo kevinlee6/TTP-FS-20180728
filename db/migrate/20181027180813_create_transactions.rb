b0fa5754b73ec87f9aaa0d4185c1d358f6f4c1ab
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-10-27 14:19:25 -0400

    Create transaction model for User and associations.

---
+++ db/migrate/20181027180813_create_transactions.rb
@@ -0,0 +1,12 @@
+class CreateTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :transactions do |t|
+      t.string :ticker, null: false
+      t.integer :num_shares, null: false
+      t.float :price_per_share, null: false
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_174945) do
+ActiveRecord::Schema.define(version: 2018_10_27_180813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "ticker", null: false
+    t.integer "num_shares", null: false
+    t.float "price_per_share", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
