aa660d05f58ed5f50c6f531a6fbe0c306cd88744
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-10-27 20:55:23 -0400

    Clean up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  protected
+  def authenticate_user
+    redirect_to new_user_registration_url unless user_signed_in?
+  end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,31 +1,18 @@
 class TransactionsController < ApplicationController
-  before_filter :sanitize_page_params
+  before_action :authenticate_user
+  before_action :sanitize_page_params
 
   def index
     @transactions = current_user.transactions
   end
 
   def create
-    # Should also update portfolio/owned shares.
     new_transaction = current_user.transactions.create!(transaction_params)
-    update_portfolio(params[:ticker], params[:qty])
-    # if new_transaction.save
-    # else
-    # end
+    update_portfolio(ticker: params[:ticker], qty: params[:qty])
   end
 
   private
-  def transaction_params
-    # look into require before permit
-    params.permit(:ticker, :qty, :price_per_share)
-  end
-
-  def sanitize_page_params
-    params[:qty] = params[:qty].to_i
-    params[:price_per_share] = params[:price_per_share].to_f
-  end
-
-  def update_portfolio(ticker, qty)
+  def update_portfolio(ticker:, qty:)
     portfolio = current_user.portfolio
 
     target_share = OwnedShare.find_by(ticker: ticker, portfolio_id: portfolio.id)
@@ -36,5 +23,16 @@ class TransactionsController < ApplicationController
     else
       OwnedShare.create!(portfolio_id: portfolio.id, num_shares: qty, ticker: ticker)
     end
+  end
+
+  def transaction_params
+    # look into require before permit
+    params.permit(:ticker, :qty, :price_per_share)
+  end
+
+  def sanitize_page_params
+    params[:ticker] = params[:ticker].upcase
+    params[:qty] = params[:qty].to_i
+    params[:price_per_share] = params[:price_per_share].to_f
   end
 end
