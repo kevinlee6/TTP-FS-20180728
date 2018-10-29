8c27fcea08243d24f8e9a6ca4c01742e0704544b
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-10-29 19:12:25 -0400

    Style user uath pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   protected
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 
   # def authenticate_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates_presence_of :name
+
   has_many :transactions
   has_one :portfolio, dependent: :destroy
 
