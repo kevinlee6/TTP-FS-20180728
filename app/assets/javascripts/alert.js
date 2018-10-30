5e71a4c13ebcec81dafd83f7cc0fb85ee23813a2
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-10-30 00:55:53 -0400

    Style flash message. Add conditional script to remove flash message.

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,12 @@
+document.addEventListener('turbolinks:load', () => {
+  // 3000ms is length of alert/flash animation
+  const alert = document.getElementsByClassName('alert');
+  const parent = document.getElementsByClassName('main-content')[0];
+  setTimeout(() => {
+    if (alert.length) {
+      for (let i = 0; i < alert.length; i++) {
+        parent.removeChild(alert[i]);
+      }
+    }
+  }, 3000); 
+});
