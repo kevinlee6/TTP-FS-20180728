5ef433cf5f43e568212b00429d4d4d0a5a9d24b1
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-11-01 03:16:39 -0400

    Authenticate users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  before_action :authenticate_user!
   before_action :configure_sign_up_params, if: :devise_controller?
 
   protected
@@ -7,8 +8,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
-
-  # def authenticate_user
-  #   redirect_to new_user_registration_url unless user_signed_in?
-  # end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
     # Homepage/Landing
   end
