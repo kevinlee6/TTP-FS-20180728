b2fd2862858a60304549e97ed6f6f6c22d154540
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2018-11-01 04:03:33 -0400

    Style homepage for logged in users.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
+
   def index
     # Homepage/Landing
+    return if !current_user
+    transactions = current_user.transactions
+
+    # Show last 5 transactions
+    @transactions = transactions.last(5).reverse()
   end
 end
