1737609b6debfcd465d47025966318fd14cdacec
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-02 23:58:17 -0500

    Switch from create! to new and save in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,26 @@ end
 tickers = %w[AAPL MSFT TSLA GOOG AMZN IBM S B AMD GE]
 prices = get_batch_price(tickers)
 
-u1 = User.create!(
+user = User.new(
   name: 'Kevin',
   email: 'kevin@gmail.com',
   password: 'password',
   cash: 50000
 )
 
-tickers.each do |ticker|
-  qty = rand(1..21)
-  u1.transactions.create!(
-    ticker: ticker,
-    qty: qty,
-    price_per_share: prices[ticker]['price'].to_f.floor(2), 
-    method: 'buy'
-  )
-  u1.portfolio.owned_shares.create!(
-    ticker: ticker,
-    num_shares: qty
-  )
+if user.save
+  u1 = User.last
+  tickers.each do |ticker|
+    qty = rand(1..21)
+    u1.transactions.create!(
+      ticker: ticker,
+      qty: qty,
+      price_per_share: prices[ticker]['price'].to_f.floor(2), 
+      method: 'buy'
+    )
+    u1.portfolio.owned_shares.create!(
+      ticker: ticker,
+      num_shares: qty
+    )
+  end
 end
